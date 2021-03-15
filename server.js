75906a68b9dc092a00463163beb0d3250c0bc88b
AyaaBe95 <75928500+AyaaBe95@users.noreply.github.com>
2021-03-15 23:07:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const methodOverride = require('method-override');
+const cors = require('cors');
+
+//server setup
+const app = express();
+
+//middlewares
+require('dotenv').config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+//database setup
+const client = new pg.Client(process.env.DATABASE_URL);
+
+// const client = new pg.Client({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: { rejectUnauthorized: false },
+// });
+
+
+const PORT = process.env.PORT || 3030;
+
+client.connect().then(() => {
+	app.listen(PORT, () => {
+		console.log(`listening on PORT ${PORT}`);
+	});
+});
 
