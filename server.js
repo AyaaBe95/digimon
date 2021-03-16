6843040486692dd9de558739b2ed4ed83c0f3b00
aya berjawi <ayabarjawi@gmail.com>
2021-03-16 22:33:27 +0200

    lab

--- server.js
+++ server.js
@@ -28,11 +28,14 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 //routes
 app.get('/',homeHandler);
-app.post('/favourite',addDataHandler);
-app.get('/favourite',addDataHandler2);
+app.post('/favourite',favouriteHandler);
+app.get('/favourite',favouriteHandler2);
 app.get('/details/:id',detailsHandler);
-app.delete('/delete/:id',deleteHandler);
 app.put('/update/:id',updateHandler);
+app.delete('/delete/:id',deleteHandler);
+
+
+
 
 
 
@@ -40,69 +43,64 @@ app.put('/update/:id',updateHandler);
 
 //handlers
 function homeHandler(req,res){
-	let url=`https://digimon-api.herokuapp.com/api/digimon`;
+	let url='https://digimon-api.herokuapp.com/api/digimon'
 	superagent.get(url)
 	.then((data)=>{
-
 		let digimonData = data.body.map((item)=>{
-			console.log(item)
 
-			return new Digimon(item)
-
+			return new Digimon(item);
 		})
 		res.render('pages/home',{data:digimonData})
 	})
 
 }
 
-function addDataHandler(req,res){
+function favouriteHandler(req,res){
 	let {name,img,level}=req.body;
-	let sql ="INSERT INTO digimon_table (name,img,level) VALUES ($1,$2,$3);";
+	let sql='INSERT INTO digimon (name,img,level) VALUES ($1,$2,$3);';
 	let values=[name,img,level];
 	client.query(sql,values)
-	.then((results)=>{
+	.then((data)=>{
 		res.redirect('/favourite')
 	})
-
 }
 
-function addDataHandler2(req,res){
-	let sql = 'SELECT * FROM digimon_table;';
-	client.query(sql).then((results) => {
-		// console.log(results.rows);
-		res.render('pages/favourite', { data: results.rows });
-	});
+function favouriteHandler2(req,res){
+	let sql='SELECT * FROM digimon;';
+	client.query(sql)
+	.then((results)=>{
+		res.render('pages/favourite',{data:results.rows});
+	})
 }
 
 function detailsHandler(req,res){
-	let id = req.params.id;
-	let sql='SELECT * FROM digimon_table WHERE id=$1'
+	let id=req.params.id;
+	let sql='SELECT * FROM digimon WHERE id=$1;';
 	let value=[id];
 	client.query(sql,value)
 	.then((results)=>{
-		res.render('pages/details',{data:results.rows[0]})
-	})
-
-}
-
-function deleteHandler(req,res){
-	let id=req.params.id;
-	let sql='DELETE FROM digimon_table WHERE id=$1;';
-	let value =[id];
-	client.query(sql,value)
-	.then((results) =>{
-		res.redirect('/favourite')
+		res.render('pages/details',{data:results.rows[0]});
 	})
 }
 
 function updateHandler(req,res){
 	let id=req.params.id;
-	let {name,img,level}=req.body;
-	let sql='UPDATE digimon_table SET name=$1,img=$2,level=$3 WHERE id=$4;';
-	let values=[name,img,level,id];
+	let{name,img,level}=req.body;
+	let sql='UPDATE digimon SET name=$1,img=$2,level=$3 WHERE id=$4;';
+	let values=[name,img,level,id]
 	client.query(sql,values)
 	.then((results)=>{
 		res.redirect(`/details/${id}`);
+	})
+}
+
+function deleteHandler(req,res){
+	let id=req.params.id;
+	let sql='DELETE FROM digimon WHERE id=$1;';
+	let value=[id];
+	client.query(sql,value)
+	.then((results)=>{
+		res.redirect('/favourite')
 	})
 
 
@@ -110,13 +108,15 @@ function updateHandler(req,res){
 
 
 
-//constractor
 
+//constractor
 function Digimon(data){
 	this.name=data.name;
 	this.img=data.img;
 	this.level=data.level;
+
 }
+
 
 
 
